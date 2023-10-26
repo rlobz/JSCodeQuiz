7e9fb543356b4d682f98d216d51cb5955245c420
rlobz <rlobzowski@gmail.com>
2023-10-26 12:11:56 +0100

    fixed errors with questions buttons and highscores

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ var four = document.getElementById("four");
 var correctIncorrect = document.getElementById("correct-incorrect");
 var highscoresPage = document.getElementById("highscorePage");
 var highscores = document.getElementById("highscores");
-var back = document.getElementById("back");
 var highscoresList = document.getElementById("highscores-list");
 var allDone = document.getElementById("allDonePage");
 var finalScore = document.getElementById("final-score");
@@ -36,32 +35,32 @@ var myQuestions = [
     {
         question: "The three fundamental programming languages of the modern web are: HTML, CSS, and _________.",
         answer: ["Dothraki", "Hebrew", "JavaScript", "HTML"],
-        correct: "JavaScript"
+        correct: 2
     },
     { 
         question: "Variables are the _______ of programming.",
         answer: ["verbs", "icebergs", "whales", "nouns"],
-        correct: "nouns"
+        correct: 3
     },
     { 
         question: "Which identifier will surround a string in JavaScript?",
         answer: ["bulbs", "lampshades", "quotation marks", "parentheses"],
-        correct: "quotation marks"
+        correct: 2
     },
     { 
         question: "Where will the \"console.log()\" method display data?",
         answer: ["in the garden", "in the browser", "in the console", "in the toolbar"],
-        correct: "in the console"
+        correct: 2
     },
     { 
         question: "When an alert is executed it will popup in the _____",
         answer: ["console", "browser", "dictionary", "atmosphere"],
-        correct: "browser"
+        correct: 1
     },
     {
         question: "empty question",
         answer: ["---", "---", "---", "---"],
-        correct: "---"
+        correct: 0
       },
 ];
 
@@ -115,19 +114,20 @@ var myQuestions = [
 //WHEN I answer a question incorrectly
 //THEN time is subtracted from the clock
 
-    function checkAnswer(userChoice) {
+     function checkAnswer(answerIndex) {
       var currentQuestion = myQuestions[questionIndex];
-      if (userChoice === currentQuestion.correct) {
+      if (answerIndex === currentQuestion.correct) {
         userScore++;
         correctIncorrect.textContent = "Correct!";
       } else {
         correctIncorrect.textContent = "Wrong!";
         timeLeft -= 10;
       }
-    
+      
       questionIndex++;
       showQuestion(questionIndex);
-    }
+}
+
     
     function endGame() {
       
@@ -145,28 +145,26 @@ var myQuestions = [
 var userHighscores = [];
 
 function addHighscore(event) {
+  var loggedHighscores = JSON.parse(localStorage.getItem("userHighscores"));
+  if (loggedHighscores !== null) {
+      userHighscores = loggedHighscores;
+  }
+
   questionsPage.style.display = "none";
   allDone.style.display = "none";
   frontPage.style.display = "none";
   highscoresPage.style.display = "block";
-  
-    highscoresList.innerHTML = "";
-  for (var j = 0; j < userHighscores.length; j++) {
-    var userHighscore = userHighscores[j];
 
-    var li = document.createElement("li");
-    li.textContent = userHighscore;
-    li.setAttribute("data-index", j);
-    highscoresList.appendChild(li);
+  highscoresList.innerHTML = "";
+  for (var j = 0; j < userHighscores.length; j++) {
+      var userHighscore = userHighscores[j];
+      var li = document.createElement("li");
+      li.textContent = userHighscore;
+      li.setAttribute("data-index", j);
+      highscoresList.appendChild(li);
   }
 }
-  var getHighscores = function() {
-  var loggedHighscores = JSON.parse(localStorage.getItem("userHighscores"));
-  if (userHighscores !== null) {
-    userHighscores = loggedHighscores;
-  }
-  addHighscore();
-}
+  
   var storeHighscore = function() {
   localStorage.setItem("userHighscores", JSON.stringify(userHighscores));
 }
@@ -181,7 +179,7 @@ submitInitials.addEventListener("click", function(event) {
   userHighscores.push(userInitialsScore);
   inputdefault.value = "";
   storeHighscore();
-  getHighscores();
+  addHighscore();
 });
 
 var clearScores = function(event) {
@@ -190,32 +188,14 @@ var clearScores = function(event) {
   console.log(userHighscores)
   highscoresList.textContent = "";
   console.log(localStorage);
-  checkHighscore();
+  addHighscore();
 }
 
     
     startButton.addEventListener("click", startQuiz);
-    
-    one.addEventListener("click", function () {
-      checkAnswer(one.textContent);
-    });
-    two.addEventListener("click", function () {
-      checkAnswer(two.textContent);
-    });
-    three.addEventListener("click", function () {
-      checkAnswer(three.textContent);
-    });
-    four.addEventListener("click", function () {
-      checkAnswer(four.textContent);
-    });
-    
-    back.addEventListener("click", frontPage);
-    highscores.addEventListener("click", checkHighscore);
+  
+    highscores.addEventListener("click", addHighscore);
     clearHighscores.addEventListener("click", clearScores);
-
-
-
-
 
 
 
